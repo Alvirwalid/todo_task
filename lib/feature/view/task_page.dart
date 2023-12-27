a38a0aeda3af841a8269ef7adb2a698b81e6ecab
Alvi <alviarash620@gmail.com>
2023-12-27 09:26:13 +0600

    Controller update

--- lib/feature/view/task_page.dart
+++ lib/feature/view/task_page.dart
@@ -222,6 +222,9 @@ class _TaskpageState extends State<Taskpage> {
                                                   color:  Colors.green,
                                                 ),
                                                 onPressed: () {
+                                                  _.titleController.text=_.taskList[index].title??'';
+                                                  _.descriptionController.text=_.taskList[index].description??'';
+                                                  _.statusController.text=_.taskList[index].status??'';
                                                   buildShowDialog(context, _, index).then((value){
 
                                                     _.titleController.clear();
@@ -300,6 +303,9 @@ class _TaskpageState extends State<Taskpage> {
                                                               padding:OtherConstant.kRegularPadding,
                                                               title:LocalString.kUpdate,
                                                               onPressed: (){
+                                                                _.titleController.text=_.taskList[index].title??'';
+                                                                _.descriptionController.text=_.taskList[index].description??'';
+                                                                _.statusController.text=_.taskList[index].status??'';
                                                                 Get.back();
                                                                 buildShowDialog(context, _, index).then((value){
                                                       
